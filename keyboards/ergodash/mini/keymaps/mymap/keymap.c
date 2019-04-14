550c54c9a5733d299eca62f1b07fc692c8944aa2
kurubushi <krbshi@gmail.com>
2019-04-15 01:52:24 +0900

    [mymap] add my keymap for ergo dash mini
    Ergo dash mini 用のキーマップを更新．
    マジで慣れるのだろうか……．

--- keyboards/ergodash/mini/keymaps/mymap/keymap.c
+++ keyboards/ergodash/mini/keymaps/mymap/keymap.c
@@ -24,33 +24,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |                    |      |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |                    |      |   N  |   M  |   ,  |   .  |   /  |  \   |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |                    |      |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Esc |  GUI |  ALt |       |||||||| Lower| Space|      ||||||||      | Enter| Raise||||||||   [  |   ]  |   `  | Shift|
+   * | Esc |  GUI |  ALt |       |||||||| Lower| Space|      ||||||||      | Enter| Raise||||||||   [  |   ]  |  ALt | Ctrl |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R, KC_T,  KC_ESC,                      KC_NO,  KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_MINUS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,  KC_F, KC_G,  KC_NO,                       KC_NO,  KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V, KC_B,  KC_NO,                       KC_NO,  KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, \
-    KC_ESC,  KC_LGUI, KC_LALT, KC_NO,       LOWER, KC_SPC, KC_NO,        KC_NO, KC_ENT, RAISE, KC_LBRC, KC_RBRC, KC_GRV, KC_RSFT \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V, KC_B,  KC_NO,                       KC_NO,  KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    KC_ESC,  KC_LGUI, KC_LALT, KC_NO,       LOWER, KC_SPC, KC_NO,        KC_NO, KC_ENT, RAISE, KC_LBRC, KC_RBRC, KC_RALT, KC_RCTL \
   ),
 
   /* Lower
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  Tab |   !  |   @  |   #  |   $  |   %  |  Esc |                    |      |   ^  |   &  |   *  |   (  |   )  | Bksp |
+   * |  Tab |   !  |   @  |   #  |   $  |   %  |  Esc |                    |      |   ^  |   &  |   *  |   (  |   )  |   \  |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   1  |   2  |   3  |   4  |   5  |      |                    |      |   -  |   =  |   \  |   `  |   [  |  ]   |
+   * | Ctrl |   1  |   2  |   3  |   4  |   5  |      |                    |      |   -  |   =  |   `  |   [  |   ]  |   |  |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|  6   |  7  |    8  |   9  |   0  |      |                    |      |   _  |   +  |  |   |   ~  |  {   |   }  |
+   * | Shift|  6   |  7  |    8  |   9  |   0  |      |                    |      |   _  |   +  |  ~   |   {  |   }  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * | Esc  |  GUI |  ALt |      |||||||| Lower| Space|      ||||||||      | Bksp | Raise|||||||| Home |PageDn|PageUp|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC, KC_ESC,                      KC_NO,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_NO,                       KC_NO,   KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_LBRC, KC_RBRC , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_NO,                       KC_NO,   KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, KC_LCBR, KC_RCBR, \
+    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC, KC_ESC,                      KC_NO,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, \
+    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_NO,                       KC_NO,   KC_MINS, KC_EQL,  KC_BSLS, KC_LBRC, KC_RBRC, KC_PIPE, \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_NO,                       KC_NO,   KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_RSFT, \
     KC_ESC,  KC_LGUI, KC_LALT, KC_NO,           LOWER,   KC_SPC, KC_NO,        KC_NO, KC_BSPC, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
