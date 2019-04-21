4d3e48a390b6e8cebf7f387223aca8c6d09c0824
kurubushi <krbshi@gmail.com>
2019-04-21 23:40:00 +0900

    [keymap] add B key into right one
    キーマップを更新．あまりにも打ち間違いが多いので B
    を右にも追加してみた．

--- keyboards/ergodash/mini/keymaps/mymap/keymap.c
+++ keyboards/ergodash/mini/keymaps/mymap/keymap.c
@@ -22,18 +22,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  Tab |   Q  |   W  |   E  |   R  |   T  |  Esc |                    |  Del |   Y  |   U  |   I  |   O  |   P  |  -   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |  Esc |                    |  Bksp|   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |  Esc |                    |   B  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      |                    |      |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Esc  |  GUI |  ALt |      |||||||| Lower| Space|      ||||||||      | Enter| Raise||||||||   [  |   ]  |  ALt | Ctrl |
+   * | Esc  |  GUI |  Alt ||||||||  GUI | Lower| Space|      ||||||||      | Enter| Raise| Bksp ||||||||      |  ALt | Ctrl |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R, KC_T,  KC_ESC,                           KC_DEL,  KC_Y,  KC_U,    KC_I,    KC_O,   KC_P,    KC_MINUS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F, KC_G,  KC_ESC,                           KC_BSPC, KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V, KC_B,  XXXXXXX,                          XXXXXXX, KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
-    KC_ESC , KC_LGUI, KC_LALT, XXXXXXX,       LOWER, KC_SPC,  XXXXXXX,        XXXXXXX, KC_ENT,  RAISE, KC_LBRC, KC_RBRC, KC_RALT, KC_RCTL \
+    KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_ESC,                           KC_DEL,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_ESC,                           KC_BSPC, KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  XXXXXXX,                          KC_B,    KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_ESC , KC_LGUI, KC_LALT,          KC_LGUI, LOWER, KC_SPC,  XXXXXXX,        XXXXXXX, KC_ENT,  RAISE, KC_BSPC,          XXXXXXX, KC_RALT, KC_RCTL \
   ),
 
   /* Lower
