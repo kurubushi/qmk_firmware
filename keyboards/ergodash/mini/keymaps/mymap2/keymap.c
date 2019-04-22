6dbf00c94fd6dd6c810b167993849df95f9fedc5
kurubushi <krbshi@gmail.com>
2019-04-23 01:37:43 +0900

    [mymap] fix qwert/dvrak switching

--- keyboards/ergodash/mini/keymaps/mymap2/keymap.c
+++ keyboards/ergodash/mini/keymaps/mymap2/keymap.c
@@ -40,20 +40,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Dvorak
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  Tab |   '  |   ,  |   .  |   P  |   Y  |DVORAK|                    |  Del |   F  |   G  |   C  |   R  |   L  |  /   |
+   * |      |   '  |   ,  |   .  |   P  |   Y  |      |                    |      |   F  |   G  |   C  |   R  |   L  |  /   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   O  |   E  |   U  |   I  |  Esc |                    |   X  |   D  |   H  |   T  |   N  |   S  |  -   |
+   * |      |   A  |   O  |   E  |   U  |   I  |      |                    |      |   D  |   H  |   T  |   N  |   S  |  -   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   ;  |   Q  |   J  |   K  |   X  |      |                    |      |   B  |   M  |   W  |   V  |   Z  | Shift|
+   * |      |   ;  |   Q  |   J  |   K  |   X  |      |                    |      |   B  |   M  |   W  |   V  |   Z  |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Esc  |  GUI |  Alt ||||||||  GUI | Lower| Space|      ||||||||      | Enter| Raise| Bksp ||||||||      |  ALt | Ctrl |
+   * |      |      |      ||||||||      |      |      |      ||||||||      |      |      |      ||||||||      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_DVORAK] = LAYOUT( \
-    KC_TAB , KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,  DVORAK,                           KC_DEL,  KC_F,  KC_G,    KC_C, KC_R,    KC_L,    KC_SLSH, \
-    KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,  KC_ESC,                           KC_X,    KC_D,  KC_H,    KC_T, KC_N,    KC_S,    KC_MINUS, \
-    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,  XXXXXXX,                          XXXXXXX, KC_B,  KC_M,    KC_W, KC_V,    KC_Z,    KC_RSFT, \
-    KC_ESC , KC_LGUI, KC_LALT,          KC_LGUI, LOWER, KC_SPC,  XXXXXXX,        XXXXXXX, KC_ENT,  RAISE, KC_BSPC,       XXXXXXX, KC_RALT, KC_RCTL \
+    _______, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    _______,                         _______, KC_F,    KC_G,    KC_C,    KC_R, KC_L,    KC_SLSH, \
+    _______, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    _______,                         KC_X,    KC_D,    KC_H,    KC_T,    KC_N, KC_S,    KC_MINUS, \
+    _______, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    _______,                         _______, KC_N,    KC_M,    KC_W,    KC_V, KC_Z,    _______, \
+    _______, _______, _______,          _______, _______, _______, _______,       _______, _______, _______, _______,          _______, _______, _______ \
   ),
 
   /* Lower
@@ -113,33 +113,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool is_dvorak = false;
 
+void layer_on_qd(int layer) {
+  if (is_dvorak) {
+    layer_off(_DVORAK);
+  }
+  return layer_on(layer);
+}
+
+void layer_off_qd(int layer) {
+  layer_off(layer);
+  if (is_dvorak) {
+    layer_on(_DVORAK);
+  }
+  return;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case LOWER:
       if (record->event.pressed) {
-        layer_on(_LOWER);
+        layer_on_qd(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
-        layer_off(_LOWER);
+        layer_off_qd(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
-        layer_on(_RAISE);
+        layer_on_qd(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
-        layer_off(_RAISE);
+        layer_off_qd(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case ADJUST:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on_qd(_ADJUST);
       } else {
-        layer_off(_ADJUST);
+        layer_off_qd(_ADJUST);
       }
       return false;
       break;
@@ -147,11 +162,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         if (is_dvorak) {
             is_dvorak = false;
-            layer_off(_DVORAK);
+            layer_off_qd(_DVORAK);
         }
         else {
             is_dvorak = true;
-            layer_on(_DVORAK);
+            layer_on_qd(_DVORAK);
         }
       }
       return false;
